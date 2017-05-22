5e1f483f572c8b18f98d23cb82f9948d3b3cf52e
deskagebunshin <almchung@ucla.edu>
2017-05-22 11:32:33 -0700

    created ML-MLoutput module

---
+++ Assets/MLModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MLModel : MonoBehaviour {
+    public int tau;
+
+    public int[][] objectArray;
+
+    // Using dummy model for now
+	void Start () {
+        // initialize ML clock
+
+        // set tau(delta t) [in number of frames]
+        tau = 10;
+        //
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DummyModel();
+	}
+
+
+    private void DummyModel()
+    {
+        // do something
+    }
+}
+
