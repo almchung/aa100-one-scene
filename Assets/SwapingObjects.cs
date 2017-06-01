adbf1f929fd6399d6890e3da1aafeebe7b82ada8
deskagebunshin <almchung@ucla.edu>
2017-06-01 16:21:15 -0700

    created swapping objects

---
+++ Assets/SwapingObjects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapingObjects : MonoBehaviour {
+    public GameObject[] prefabs;
+    public int selectedModel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
